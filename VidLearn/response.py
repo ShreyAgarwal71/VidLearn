0a1ac24d0cc86f05a245499c429e2f3de6e1ed81
Kayshav Bhardwaj <67803250+KayshavBhardwaj@users.noreply.github.com>
2025-02-21 22:06:30 -0800

    Update response.py
    added newer model (2.0 flash) instead of older model (1.5 8b)

--- VidLearn/response.py
+++ VidLearn/response.py
@@ -3,13 +3,13 @@ from google.genai import types
 
 import PIL.Image
 
-image = PIL.Image.open('/path/to/image.png')
+image = PIL.Image.open('PNG/path/here.png')
 
-client = genai.Client(api_key="GEMINI_API_KEY")
+client = genai.Client(api_key="AIzaSyCZDI2WQbEAVhWH7AxasayTdkBK5yA5Uk8")
 response = client.models.generate_content(
 
-    # model="gemini-2.0-flash",
-    model="gemini-1.5-flash-8B",
-    contents=["What is this image?", image])
+    model="gemini-2.0-flash",
+    # model="gemini-1.5-flash-8b-exp-0827",
+    contents=["INSERT PROMPT HERE", image])
 
 print(response.text)
