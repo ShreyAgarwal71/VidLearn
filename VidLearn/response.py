853f6207f548d10242389366e1a4bf18fe84ced2
Kayshav Bhardwaj <67803250+KayshavBhardwaj@users.noreply.github.com>
2025-02-22 00:11:01 -0500

    Add files via upload

---
+++ VidLearn/response.py
@@ -0,0 +1,15 @@
+from google import genai
+from google.genai import types
+
+import PIL.Image
+
+image = PIL.Image.open('/path/to/image.png')
+
+client = genai.Client(api_key="GEMINI_API_KEY")
+response = client.models.generate_content(
+
+    # model="gemini-2.0-flash",
+    model="gemini-1.5-flash-8B",
+    contents=["What is this image?", image])
+
+print(response.text)
